21d905e7b592e4c53e22864e8ea497a17baeab1d
Andrew Jaffe <adjaffeb@gmail.com>
2025-08-11 18:41:36 -0400

    Update aperio to v0.0.2

--- Formula/aperio.rb
+++ Formula/aperio.rb
@@ -1,18 +1,18 @@
 class Aperio < Formula
   desc "A blazing-fast, zero-dependency CLI for beautiful file statistics"
   homepage "https://github.com/ADJB1212/Aperio"
-  url "https://github.com/ADJB1212/Aperio/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "96c98e45173208f4f7bf67b5ffdfaf416b80790630b90ce0c633eb0a18680683"
+  url "https://github.com/ADJB1212/Aperio/archive/refs/tags/v0.0.2.tar.gz"
+  sha256 "b8cab0c721fcddb22062215a17120722da39bb92ea66383ca2ad5be4817a9862"
   license "GPL-3.0-or-later"
   head "https://github.com/ADJB1212/Aperio.git", branch: "main"
-  version "0.0.1"
+  version "0.0.2"
 
   depends_on "go" => :build
 
   def install
     Dir.chdir("cmd/aperio") do
         ENV["CGO_ENABLED"] = "0"
-        system("go build -buildvcs=false -o=aperio -ldflags=\"-s -w -X main.version=0.0.1\"")
+        system("go build -buildvcs=false -o=aperio -ldflags=\"-s -w -X main.version=0.0.2\"")
         bin.install "aperio"
     end
 
